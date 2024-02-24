9e8d348e46118a7ec55d40b5fd15b0ea11d54e28
K <104524788+Keywos@users.noreply.github.com>
2024-02-25 05:11:15 +0800

    [-] top

--- script/ithome/js/index.js
+++ script/ithome/js/index.js
@@ -3,18 +3,18 @@ let url = $request.url,
   i = JSON.parse($response.body),
   FeedTypes = [10023], //直播tip
   banner = true,
-  top = true,
+  tops = true,
   bannerAd = true;
 
 if (isLoon) {
   bannerAd = $persistentStore.read("去除轮播图广告") === "开启";
   banner = $persistentStore.read("去除整个轮播图") === "开启";
-  top = $persistentStore.read("去除置顶") === "开启";
+  tops = $persistentStore.read("去除置顶") === "开启";
 } else if (typeof $argument !== "undefined" && $argument !== "") {
   const ins = getin("$argument");
   bannerAd = ins.bannerAd != 0;
   banner = ins.banner != 0;
-  top = ins.top != 0;
+  tops = ins.top != 0;
 }
 
 if (/api\/douyin\/GetLiveInfo/.test(url)) {
@@ -38,7 +38,7 @@ if (/api\/douyin\/GetLiveInfo/.test(url)) {
     }
   }
   banner && FeedTypes.push(10002); //轮播
-  top && FeedTypes.push(10003); //置顶
+  tops && FeedTypes.push(10003); //置顶
   i.data.list = i.data.list.filter((item) => {
     return (
       !FeedTypes.includes(item.feedType) &&
@@ -50,3 +50,4 @@ $done({ body: JSON.stringify(i) });
 
 // prettier-ignore
 function getin() {return Object.fromEntries($argument.split("&").map((i) => i.split("=")).map(([k, v]) => [k, decodeURIComponent(v)]));}
+
